2b2cd8bf0c6d5092eccba9b7067725343e9ab6f2
flex-57 <129852644+flex-57@users.noreply.github.com>
2024-10-15 16:54:46 +0200

    Ajout Solution + TP Yaourts

---
+++ Yaourts/Program.cs
@@ -0,0 +1,20 @@
+﻿using HttpClient client = new();
+
+namespace Yaourts
+{
+    internal class Program
+    {
+        static async Task ProcessRepositoriesAsync(HttpClient client)
+        {
+            await ProcessRepositoriesAsync(client);
+            var json = await client.GetStringAsync(
+                "https://api.devoldere.net/polls/yoghurts/");
+
+            Console.Write(json);
+        }
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
